e2337449d1e8fbdb99cdb7bdfd3be5918f8ab297
nbogie <neillbogie@googlemail.com>
2022-01-15 03:23:27 +0000

    add first (failing) unit test (for trickier scoring)

---
+++ src/scoring.test.tsx
@@ -0,0 +1,14 @@
+import { ScoreCategory, ScoredLetter, scoreGuess } from './scoring';
+
+it("prioritises later green when processing first of a double letter in input",
+    () => {
+        const res: ScoredLetter[] = scoreGuess("LADLE", "STALE");
+        const expected: ScoreCategory[] = [
+            'notInWord',
+            'partial',
+            'notInWord',
+            'placed',
+            'placed'
+        ]
+        expect(res.map(s => s.score)).toEqual(expected);
+    });

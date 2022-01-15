2db59b2ec7857a3a6019806f4392189d3c19a1fc
nbogie <neillbogie@googlemail.com>
2022-01-15 02:24:43 +0000

    keyboard enter and backspace.  +new game.

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -2,23 +2,48 @@ import { LetterStates } from "../types";
 
 interface KeyboardProps {
     letterStates: LetterStates;
-    handleLetterEntry: (k: string) => void;
+    handleLetterEntry: (k: KeyboardKey) => void;
 }
+type SpecialKey = {
+    type: 'special',
+    display: string,
+    effect: 'Enter' | 'Backspace'
+};
+type LetterKey = { type: 'letter', letter: string };
+export type KeyboardKey = LetterKey | SpecialKey;
+
 export default function Keyboard(props: KeyboardProps) {
-    const keysForRows: string[][] = ['ABCDEF', 'GHIJK', 'LMNOPQ', 'RSTUV', '1WXYZ2'].map(word => word.split(''));
+
+    function makeKey(shorthand: string): KeyboardKey {
+        if (shorthand === '1') {
+            return { type: 'special', display: 'Enter', effect: 'Enter' };
+        }
+        if (shorthand === '2') {
+            return { type: 'special', display: 'DEL', effect: 'Backspace' };
+        }
+        return { type: 'letter', letter: shorthand };
+    }
+
+    function keyToString(k: KeyboardKey): string {
+        return k.type === 'letter' ? k.letter : k.display;
+    }
+
+    const keysForRows: KeyboardKey[][] = ['ABCDEF', 'GHIJK', 'LMNOPQ', 'RSTUV', '1WXYZ2'].map(w => w.split('').map(makeKey));
 
     return <div className='keyboard'>
 
         {
-            keysForRows.map((keysForRow: string[], ix) => (
+            keysForRows.map((keysForRow: KeyboardKey[], ix) => (
                 <div
                     className='keyboardRow'
                     key={ix}>
                     {keysForRow.map(k => (
                         <button
-                            className={'keyboardKey ' + props.letterStates[k]}
-                            key={k}
-                            onClick={(ev) => props.handleLetterEntry(k)}>{k}</button>
+                            className={'keyboardKey ' + (k.type === 'letter' ? props.letterStates[k.letter] : '')}
+                            key={k.type === 'letter' ? k.letter : k.display}
+                            onClick={(ev) => props.handleLetterEntry(k)}>
+                            {keyToString(k)}
+                        </button>
                     ))}
                 </div>
             ))
--- src/components/WordMindGame.tsx
+++ src/components/WordMindGame.tsx
@@ -2,15 +2,20 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { GuessView, PlaceholderGuessView } from './GuessView';
 import { Guess, LetterState, LetterStates } from '../types';
 import { randomWord } from './wordList';
-import Keyboard from './Keyboard';
+import Keyboard, { KeyboardKey } from './Keyboard';
 import { ScoreCategory, ScoredLetter, scoreGuess } from '../scoring';
 
 function WordMindGame() {
     const maxGuesses = 6;
-    const [wordToGuess,] = useState(randomWord());
-
+    const [wordToGuess, setWordToGuess] = useState(randomWord());
     const [currentGuess, setCurrentGuess] = useState('');
-    const [previousGuesses,] = useState<Guess[]>([]);
+    const [previousGuesses, setPreviousGuesses] = useState<Guess[]>([]);
+
+    function resetGameWithNewWord() {
+        setWordToGuess(randomWord());
+        setCurrentGuess('');
+        setPreviousGuesses([]);
+    }
 
     function lastGuess() {
         return previousGuesses.length === 0 ? null : previousGuesses[previousGuesses.length - 1];
@@ -45,31 +50,33 @@ function WordMindGame() {
         }
     }, [currentGuess, previousGuesses])
 
-    const handleLetterEntry = useCallback(function (key: string) {
-        if (key === 'Enter') {
+    const handleLetterEntry = useCallback(function (key: KeyboardKey) {
+        if (key.type === 'special' && key.effect === 'Enter') {
             handleSubmit();
             return;
         }
-        // You don't get a key of Backspace with 'keypress' event, 
-        // unlike with 'keydown'
-        if (key === 'Backspace') {
+        if (key.type === 'special' && key.effect === 'Backspace') {
             handleBack();
             return;
         }
-        if (key) {
-            const upcased = key.toUpperCase();
+        if (key.type === 'letter') {
+            const upcased = key.letter.toUpperCase();
             if (upcased.match(/^[ABCDEFGHIJKLMNOPQRSTUVWXYZ]$/)) {
                 if (currentGuess.length < 5) {
                     setCurrentGuess(g => g + upcased)
                 }
             }
         }
+
     }, [currentGuess, handleSubmit]);
 
     useEffect(() => {
-        const listener = (e: any) => handleLetterEntry(e.key);
+        const listener = (e: KeyboardEvent) => (e.key === 'Enter' || e.key === 'Backspace') ?
+            handleLetterEntry({ type: 'special', display: e.key, effect: e.key }) :
+            handleLetterEntry({ type: 'letter', letter: e.key });
 
         function registerKeyListener() {
+            //keydown, not keypress - no backspace event.
             window.addEventListener('keydown', listener);
         }
         registerKeyListener();
@@ -78,12 +85,16 @@ function WordMindGame() {
     }, [handleLetterEntry]);
 
     const placeholderGuesses: null[] = isGameOver ? [] : [null, null, null, null, null].slice(0, turnsRemaining - 1);
+
+    function NewGameButton() {
+        return <button className='newGame' onClick={resetGameWithNewWord}>New Game</button>
+    }
     return (
 
         <div className='wordMindGame'>
             <h2>A copy of Wordle - made with React</h2>
 
-            {playerWon() && <h3>You win!</h3>}
+            {playerWon() && <><h3>You win!</h3><NewGameButton /></>}
 
             <div className={'guessRows'}>
                 {previousGuesses.map((guess, ix) => <GuessView
@@ -100,23 +111,14 @@ function WordMindGame() {
                     key={ix}
                 />)}
 
-                {
-                    !isGameOver &&
-                    <>
-                        <div className='controls'>
-                            <button onClick={handleClear} >Clear</button>
-                            <button onClick={handleBack} >Back</button>
-                            <button onClick={handleSubmit} >Submit</button>
-                            <br />
-                        </div>
-                    </>
-                }
                 {isGameOver && !playerWon() && <><h3>Game Over.  The word was:</h3>
                     <div className='guessRow'>
                         {wordToGuess.split('').map((letter, ix) => (
                             <div key={ix} className='unscoredLetter'>{letter}</div>)
                         )}
                     </div>
+                    <br />
+                    <NewGameButton />
                 </>
                 }
 
@@ -124,17 +126,12 @@ function WordMindGame() {
             <Keyboard
                 letterStates={letterStates}
                 handleLetterEntry={handleLetterEntry} />
-            <h3>Keyboard Controls</h3>
-            <p>Type letters to add to guess<br />
-                Type 'Enter' to submit a guess<br />
-                Type backspace to remove a character
-            </p>
 
-            <h3>About Wordle</h3>
+            <h3>About the real Wordle</h3>
             <p>The real Wordle is better and can be found at<br />
                 <a href='https://www.powerlanguage.co.uk/wordle/'>https://www.powerlanguage.co.uk/wordle/</a>
                 <br />
-                The version here is just for study purposes.
+                The version here is just for study purposes, though it does let you play multiple times per day!
             </p>
         </div >
     )

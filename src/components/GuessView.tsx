c534696ce30470060e85acfbea35930ddc3dff1d
nbogie <neillbogie@googlemail.com>
2022-01-15 04:53:03 +0000

    add a rough reveal anim w framer-motion

--- src/components/GuessView.tsx
+++ src/components/GuessView.tsx
@@ -1,6 +1,7 @@
 import { PlaceholderLetterView, ScoredLetterView } from "./ScoredLetter";
 import { Guess } from "../types";
 import { scoreGuess } from "../scoring";
+import { motion } from 'framer-motion';
 
 interface GuessViewProps {
     guess: Guess;
@@ -10,12 +11,28 @@ interface GuessViewProps {
 export function GuessView(props: GuessViewProps) {
     const cells = scoreGuess(props.guess, props.target);
 
+    const variants = {
+        container: {
+            animate: {
+                transition: {
+                    staggerChildren: 0.3
+                }
+            }
+        }
+    };
+
     return (
-        <div className='guessRow'>
-            {cells.map((scoredLetter, ix) => <ScoredLetterView
-                scoredLetter={scoredLetter}
-                key={ix} />)}
-        </div>
+        <motion.div
+            initial="initial"
+            animate="animate"
+            variants={variants.container}
+        >
+            <div className='guessRow'>
+                {cells.map((scoredLetter, ix) => <ScoredLetterView
+                    scoredLetter={scoredLetter}
+                    key={ix} />)}
+            </div>
+        </motion.div>
     );
 }
 
--- src/components/ScoredLetter.tsx
+++ src/components/ScoredLetter.tsx
@@ -1,11 +1,27 @@
+import { motion } from "framer-motion";
 import { ScoreCategory, ScoredLetter } from "../scoring";
 
 export interface ScoredLetterProps {
     scoredLetter: ScoredLetter;
 }
 export function ScoredLetterView(props: ScoredLetterProps) {
-    return <div className={'scoredLetter ' + classForScore(props.scoredLetter.score)}>
-        {props.scoredLetter.letter}</div>;
+
+    const variants = {
+        initial: {
+            rotateY: 90
+        },
+        animate: {
+            rotateY: 0
+        }
+    }
+
+    return (
+        <motion.div variants={variants}>
+            <div className={'scoredLetter ' + classForScore(props.scoredLetter.score)}>
+                {props.scoredLetter.letter}
+            </div>
+        </motion.div>
+    );
 }
 export function PlaceholderLetterView() {
     return <div className='placeholderLetter'>

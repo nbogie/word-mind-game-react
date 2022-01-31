62c73df1c0fa36199ecc2b223518567932571d75
nbogie <neillbogie@googlemail.com>
2022-01-31 15:04:18 +0000

    implemented proper scoring

--- src/scoring.test.tsx
+++ src/scoring.test.tsx
@@ -1,14 +1,56 @@
+import { fail } from 'assert';
 import { ScoreCategory, ScoredLetter, scoreGuess } from './scoring';
 
-it.skip("prioritises later green when processing first of a double letter in input",
-    () => {
-        const res: ScoredLetter[] = scoreGuess("LADLE", "STALE");
-        const expected: ScoreCategory[] = [
-            'notInWord',
-            'partial',
-            'notInWord',
-            'placed',
-            'placed'
-        ]
+function checkScoring(guess: string, expectedScoreShorthand: string, target: string) {
+    const res: ScoredLetter[] = scoreGuess(guess, target);
+    type ShorthandChar = '0' | '1' | '2';
+    const lookup: Record<ShorthandChar, ScoreCategory> = {
+        '0': 'notInWord',
+        '1': 'partial',
+        '2': 'placed',
+    }
+
+    const shorthands = expectedScoreShorthand.split('');
+    if (!shorthands.every(ch => ['0', '1', '2'].includes(ch))) {
+        fail("expectedScore shorthand was invalid: " + expectedScoreShorthand);
+    } else {
+        const expected: ScoreCategory[] = (shorthands as ShorthandChar[]).map(digit => lookup[digit]);
         expect(res.map(s => s.score)).toEqual(expected);
-    });
+    }
+}
+
+it("should score HOOPS 22001 for HOUSE",
+    () => {
+        checkScoring('HOOPS', '22001', 'HOUSE');
+    }
+);
+
+it("should only mark ONE of a double letter as yellow in input when there's only ONE in target.  STEEP for HOUSE should be 10100",
+    () => {
+        checkScoring('STEEP', '10100', 'HOUSE');
+    }
+);
+
+
+it("prioritises later green when processing first of a double letter in input",
+    () => {
+        checkScoring('LADLE', '01022', 'STALE');
+    }
+);
+
+it("should mark a matching word correctly",
+    () => {
+        checkScoring("TRAIN", "22222", "TRAIN")
+    }
+);
+it("should mark a matching word correctly with doubles",
+    () => {
+        checkScoring("CHESS", "22222", "CHESS")
+    }
+);
+
+it("should score 00000 for a complete miss",
+    () => {
+        checkScoring("ABCDE", "00000", "TOUGH")
+    }
+);
--- src/scoring.tsx
+++ src/scoring.tsx
@@ -1,31 +1,59 @@
 import { Guess } from "./types";
 
-export function scoreGuess(guess: Guess, target: string): ScoredLetter[] {
-    return zip(guess.split(''), target.split('')).map(([gLetter, tLetter]) => scoreLetter(gLetter, tLetter, target))
-}
-
-function zip<T>(arr1: T[], arr2: T[]): [T, T][] {
-    return arr1.map((el, ix) => [el, arr2[ix]]);
-}
-
 export type ScoreCategory = 'placed' | 'partial' | 'notInWord';
 export interface ScoredLetter {
     letter: string;
     score: ScoreCategory;
 }
-export function scoreLetter(guessLetter: string, targetLetter: string, target: string): ScoredLetter {
-    let score: ScoreCategory;
 
-    if (guessLetter === targetLetter) {
-        score = 'placed';
-    } else {
-        if (target.includes(guessLetter)) {
-            score = 'partial';
-        }
-        else {
-            score = 'notInWord';
+export function scoreGuess(guess: Guess, target: string): ScoredLetter[] {
+
+    interface TargetTileBeingMarked {
+        letter: string;
+        isAccountedFor: boolean;
+    }
+
+    interface MaybeScoredLetter {
+        letter: string;
+        score?: ScoreCategory;
+    }
+    //sweep 1: account for any greens first:
+    //    for each letter guess-target pair that perfectly match
+    //        mark the target letter tile as accounted for
+    //        mark the guess letter tile as accounted for
+    //sweep 2: deal with any yellows.
+    //    for each GUESS_TILE of remaining tiles in guess
+    //        if GUESS_TILE letter is in remaining unaccounted-for tiles in TARGET
+    //        mark GUESS_TILE yellow
+    //        mark TARGET_TILE accounted for    
+
+    const guessTiles: MaybeScoredLetter[] = guess.split('').map(ch => ({ letter: ch }))
+    const targetTiles: TargetTileBeingMarked[] = target.split('').map(ch => ({ letter: ch, isAccountedFor: false }))
+    const alignedPairs = zip(guessTiles, targetTiles);
+    const perfectMatches = alignedPairs.filter(([gTile, tTile]) => gTile.letter === tTile.letter);
+    perfectMatches.forEach(([gTile, tTile]) => {
+        gTile.score = 'placed'
+        tTile.isAccountedFor = true;
+    });
+
+    const remainingGuessTiles = guessTiles.filter(gt => !gt.score);
+
+    for (let guessTile of remainingGuessTiles) {
+        const unmatchedTargetTiles = targetTiles.filter(tt => !tt.isAccountedFor);
+        const foundTargetTile = unmatchedTargetTiles.find(tt => guessTile.letter === tt.letter);
+        if (foundTargetTile) {
+            guessTile.score = 'partial';
+            foundTargetTile.isAccountedFor = true;
+        } else {
+            guessTile.score = 'notInWord';
         }
     }
-    return { letter: guessLetter, score }
+
+    //TODO: better ts to ensure this type is correct
+    return guessTiles.filter(gt => gt.score) as ScoredLetter[];
+}
+
+function zip<S, T>(arr1: S[], arr2: T[]): [S, T][] {
+    return arr1.map((el, ix) => [el, arr2[ix]]);
 }
 

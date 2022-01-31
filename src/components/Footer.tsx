1b8b29f138b127d42f9ebbebe68a035d42867b44
nbogie <neillbogie@googlemail.com>
2022-01-31 15:45:15 +0000

    add limitations in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,11 +4,19 @@ export function Footer() {
     return (
         <>
             <h3>This is not the real Wordle</h3>
-            <p>The real Wordle is better and can be found at<br />
+            <div>The real Wordle is better and can be found at<br />
                 <a href='https://www.powerlanguage.co.uk/wordle/'>https://www.powerlanguage.co.uk/wordle/</a>
                 <br />
+                <br />
                 The version here is just for a programming study, though it does let you play multiple times per day!
-            </p>
+                <br />
+                <br />
+                <strong>Known limitations of this version:</strong>
+                <br />
+                1. If you reload this page you will lose your game-in-progress and a new target word will be generated.
+                <br />
+                2. The words you submit are not checked as being legal dictionary words. (You'll have to keep yourself honest!)
+            </div>
         </>
     );
 }
